cbe596a17e4650f1687aea73585aaf0d2767a928
menxu <menxu_work@163.com>
2013-11-11 16:57:08 +0800

    add courses tab

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130925074706) do
+ActiveRecord::Schema.define(:version => 20131111085351) do
 
   create_table "blogs", :force => true do |t|
     t.integer  "user_id"
@@ -43,6 +43,14 @@ ActiveRecord::Schema.define(:version => 20130925074706) do
 
   add_index "comments", ["blog_id"], :name => "index_comments_on_blog_id"
   add_index "comments", ["user_id"], :name => "index_comments_on_user_id"
+
+  create_table "courses", :force => true do |t|
+    t.string   "name"
+    t.string   "cid"
+    t.text     "desc"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "file_uploaders", :force => true do |t|
     t.string   "title"
